2d37092b6078716a813e60199d34dbac543ce52a
Sagar Lakhia <sagarlakhia18@gmail.com>
2018-03-14 21:00:39 -0700

    Added get todos route

--- server/server.js
+++ server/server.js
@@ -20,6 +20,17 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({
+            statusCode: 200,
+            todos
+        });
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
+
 app.listen(3000, () => {
     console.log('Started on port 3000');
 })
