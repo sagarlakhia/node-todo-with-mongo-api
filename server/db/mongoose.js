80f50b5cd4b5aa5e8458543e59ac030393eff11a
Sagar Lakhia <sagarlakhia18@gmail.com>
2018-03-18 18:16:31 -0700

    removed log

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-console.log(dbcredentials.dbUsername);
 //mongoose.connect('mongodb://localhost:27017/TodoApp');
 mongoose.connect(`mongodb://todouser:todopassword@ds117729.mlab.com:17729/todoapp`)
 module.exports = {mongoose};
