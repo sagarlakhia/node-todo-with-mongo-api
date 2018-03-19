1effb97addaf6c86b64efcc60929b8ec8ce24d73
Sagar Lakhia <sagarlakhia18@gmail.com>
2018-03-18 18:13:07 -0700

    Removed credential file

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,7 @@
 var mongoose = require('mongoose');
-var dbcredentials = require('../dbcredentials');
 
 mongoose.Promise = global.Promise;
 console.log(dbcredentials.dbUsername);
 //mongoose.connect('mongodb://localhost:27017/TodoApp');
-mongoose.connect(`mongodb://${dbcredentials.dbUsername}:${dbcredentials.dbPassword}@ds117729.mlab.com:17729/todoapp`)
+mongoose.connect(`mongodb://todouser:todopassword@ds117729.mlab.com:17729/todoapp`)
 module.exports = {mongoose};
