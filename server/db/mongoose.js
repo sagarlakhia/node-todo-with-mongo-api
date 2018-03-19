cce8275c681720c1ca2cd9875fff91b4b13d0b4e
Sagar Lakhia <sagarlakhia18@gmail.com>
2018-03-18 17:53:20 -0700

    Setup for heroku and added a file for storing mlba credentials

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
+var dbcredentials = require('../dbcredentials');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-
+console.log(dbcredentials.dbUsername);
+//mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(`mongodb://${dbcredentials.dbUsername}:${dbcredentials.dbPassword}@ds117729.mlab.com:17729/todoapp`)
 module.exports = {mongoose};
