7c4b1655e9ba3ad362894d14f35a4d1b3dbbd61d
Sagar Lakhia <sagarlakhia18@gmail.com>
2018-03-26 19:28:16 -0700

    Added hashing

--- server/server.js
+++ server/server.js
@@ -108,10 +108,13 @@ app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
     var user = new User(body);
 
-    user.save().then((usr) => {
-        res.send(usr);
+    user.save().then((user) => {
+        return user.generateAuthToken();
+        
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
     }).catch((e) => {
-        res.send(400).send(e);
+        res.status(400).send(e);
     });
 });
 
